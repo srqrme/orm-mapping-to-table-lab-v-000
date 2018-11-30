a393ac26bfaace0c34ad92c3dc5d9254af9b8ec5
Sharmaine Requerme <srqrme@gmail.com>
2018-11-30 03:43:36 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,21 @@ class Student
   end
 
   def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
 
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students
+      SQL
+    DB[:conn].execute(sql)
   end
 
 end
